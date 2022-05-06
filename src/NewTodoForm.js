3836b9ef10ba585703306168f146a5788989d41b
Naumov Yura <naumoviiury@gmail.com>
2022-05-06 23:52:40 +0500

    Changed id on classNames and used flex instead float

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -45,8 +45,8 @@ function NewTodoForm(props) {
     return (
         <div id="new-todo">
             <form onSubmit={formSubmitHandler}>
-                <h2>Add new to do</h2>
-                <div id="white-div">
+                <h2>New todo</h2>
+                <div className="addNewTodoForm">
                     <label htmlFor="new-todo-header">*Title</label>
                     <input
                         className={titleError ? "error" : ""}
@@ -69,7 +69,6 @@ function NewTodoForm(props) {
                         <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M8.25 0.75H5.75V5.75H0.75V8.25H5.75V13.25H8.25V8.25H13.25V5.75H8.25V0.75Z" fill="white" />
                         </svg>
-                        Add
                     </button>
                 </div>
             </form>
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,21 +1,23 @@
 import "./TodoList.css";
 import TodoListElement from "./TodoListElement";
+import todo from "./images/todo.png";
 export default TodoList;
 
 function TodoList(props) {
     return (
         <div id="todo-list">
-            <h2>To do list</h2>
-            <div id="todos">
+            <div className="todoLogo">
+                <img src={todo} alt="Todo"></img>
+                <h2>Todo list</h2>
+            </div>
+            <div className="todos">
                 {props.todos.length ? (
                     props.todos.map((todo) => (
-                        <TodoListElement
-                            id={todo.id}
-                            key={todo.id}
-                            title={todo.title}
-                            content={todo.content} 
-                            onRemoveTodo={props.onRemoveTodo}
-                        /> ))) : (<div id="epmtyList">Список пуст...</div>)}
+                        <TodoListElement id={todo.id} key={todo.id} title={todo.title} content={todo.content} onRemoveTodo={props.onRemoveTodo} />
+                    ))
+                ) : (
+                    <div id="epmtyList">Список пуст...</div>
+                )}
             </div>
         </div>
     );
