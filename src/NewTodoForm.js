ded5ed56748773714ae51b53c66a00929555ba47
Naumov Yura <naumoviiury@gmail.com>
2022-05-06 23:04:27 +0500

    Changed icons

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -66,6 +66,9 @@ function NewTodoForm(props) {
                         cols="4"
                     ></textarea>
                     <button type="submit" id="add-todo">
+                        <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M8.25 0.75H5.75V5.75H0.75V8.25H5.75V13.25H8.25V8.25H13.25V5.75H8.25V0.75Z" fill="white" />
+                        </svg>
                         Add
                     </button>
                 </div>
